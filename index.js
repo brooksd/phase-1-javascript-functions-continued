1dd1160c49c9f758d658755324b54b4c45bf4bae
Donald Brooks Arum <brooksd079@gmail.com>
2022-06-07 14:46:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+//saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun("bathe my dog!");
+
+// mondayWork funtion
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+mondayWork("work from home");
+
+//wrapObjective
+
+function wrapAdjective(style = "*") {
+    return function(adjective = "special") {
+        return `You are ${style}${adjective}${style}!`;    }
+}
